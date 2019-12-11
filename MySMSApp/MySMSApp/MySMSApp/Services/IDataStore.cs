39fb9eced10196beead79260302466999cb87ad9
Olga <nimfaolya@mail.ru>
2019-12-11 20:11:18 +0100

    - added interface to be used with models with REST service

---
+++ MySMSApp/MySMSApp/MySMSApp/Services/IDataStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySMSApp.Services
+{
+    // interface presenting main functions like get, update, delete, get one...
+    interface IDataStore<T>
+    {
+        Task<bool> AddItemAsync(T item);
+        Task<bool> UpdateItemAsync(T item);
+        Task<bool> DeleteItemAsync(string id);
+        Task<T> GetItemAsync(string id);
+        Task<IEnumerable<T>> GetItemsAsync(bool forceRefresh = false);
+    }
+}
